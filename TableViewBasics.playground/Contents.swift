0c4fbebdfa18a61bb72c46e93783692bb69b88c0
DopeKS <kyusang.dev@gmail.com>
2024-05-10 10:18:17 +0900

    TableViewBasics

---
+++ TableViewBasics.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+import PlaygroundSupport
+
+class TableViewExampleController: UIViewController {
+    var tableView: UITableView?
+    var journalEntries: [[String]] = [
+        ["sun.max", "9 Apr 2023", "Nice weather today"],
+        ["cloud.rain", "10 Apr 2023", "Heavy rain today"],
+        ["cloud.sun", "11 Apr 2023", "It's cloudy out"]
+    ]
+}
