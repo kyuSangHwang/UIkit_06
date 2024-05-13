aeb297fa2ff45aef4be4e92e569527958c181ee6
DopeKS <kyusang.dev@gmail.com>
2024-05-13 09:48:58 +0900

    탭바 아이템 추가

--- JRNL-Codeonly/JRNL-Codeonly/SceneDelegate.swift
+++ JRNL-Codeonly/JRNL-Codeonly/SceneDelegate.swift
@@ -22,9 +22,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let journalListViewController = JournalListViewController()
         let firstNavigationController = UINavigationController(rootViewController: journalListViewController)
+        firstNavigationController.tabBarItem = UITabBarItem(title: "Journal", image: UIImage(systemName: "person.fill"), tag: 0)
         
         let mapViewController = MapViewController()
         let secondNavigationController = UINavigationController(rootViewController: mapViewController)
+        secondNavigationController.tabBarItem = UITabBarItem(title: "Map", image: UIImage(systemName: "map"), tag: 0)
         
         let tabBarController = UITabBarController()
         tabBarController.viewControllers = [firstNavigationController, secondNavigationController]
