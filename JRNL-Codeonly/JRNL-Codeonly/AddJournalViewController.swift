622a39e2a565f8c49128edaa8af14bfb61bc3e62
DopeKS <kyusang.dev@gmail.com>
2024-05-13 16:24:43 +0900

    New Entry 페이지 스택뷰 추가(cont.)

--- JRNL-Codeonly/JRNL-Codeonly/AddJournalViewController.swift
+++ JRNL-Codeonly/JRNL-Codeonly/AddJournalViewController.swift
@@ -9,6 +9,39 @@ import UIKit
 
 class AddJournalViewController: UIViewController {
     
+    private lazy var mainContainer: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 40
+        
+        return stackView
+    }()
+    
+    private lazy var ratingView: UIStackView = {
+        let stackView = UIStackView(frame: CGRect(x: 0, y: 0, width: 252, height: 44))
+        stackView.axis = .horizontal
+        stackView.backgroundColor = .systemRed
+        
+        return stackView
+    }()
+    
+    private lazy var toggleView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        
+        let switchComponent = UISwitch()
+        let labelComponent = UILabel()
+        labelComponent.text = "Switch Label"
+        stackView.addArrangedSubview(switchComponent)
+        stackView.addArrangedSubview(labelComponent)
+        
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,6 +55,26 @@ class AddJournalViewController: UIViewController {
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel,
                                                            target: self,
                                                            action: #selector(cancel))
+        
+        mainContainer.addArrangedSubview(ratingView)
+        mainContainer.addArrangedSubview(toggleView)
+        
+        view.addSubview(self.mainContainer)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        mainContainer.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mainContainer.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
+            mainContainer.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: 20),
+            mainContainer.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            mainContainer.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            
+            ratingView.widthAnchor.constraint(equalToConstant: 252),
+            ratingView.heightAnchor.constraint(equalToConstant: 44)
+        ])
+        
     }
     
     @objc func save() {
