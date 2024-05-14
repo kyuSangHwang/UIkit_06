84228a32c050b56b9147e851ca165f1c070f3ed4
DopeKS <kyusang.dev@gmail.com>
2024-05-14 14:14:17 +0900

    MapViewController 추가

---
+++ JRNL/JRNL/Map Screen/MapViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MapViewController.swift
+//  JRNL
+//
+//  Created by 황규상 on 5/14/24.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        self.navigationItem.title = "Loading..."
+        locationManager.requestLocation()
+    }
+
+    // MARK: - CLLocationManagetDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let myLocation = locations.first {
+            let lat = myLocation.coordinate.latitude
+            let long = myLocation.coordinate.longitude
+            self.navigationItem.title = "Map"
+            mapView.region = setInitialRegion(lat: lat, long: long)
+        }
+    }
+    
+    // MARK: - Methods
+    func setInitialRegion(lat: CLLocationDegrees, long: CLLocationDegrees) -> MKCoordinateRegion {
+        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: long), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    }
+
+}
