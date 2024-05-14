35f88984c324f85d06186d093c2b6306bf79f763
DopeKS <kyusang.dev@gmail.com>
2024-05-14 13:07:13 +0900

    Sync

--- JRNL/JRNL/Journal List Screen/JournalListViewController.swift
+++ JRNL/JRNL/Journal List Screen/JournalListViewController.swift
@@ -8,24 +8,68 @@
 import UIKit
 
 class JournalListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    // MARK: - Properties
     @IBOutlet var tableView: UITableView!
+    var sampleJournalEntryData = SampleJournalEntryData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        sampleJournalEntryData.createSampleJournalEntryData()
     }
 
+    // MARK: - UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10 // section에 row가 몇 개냐
+        sampleJournalEntryData.journalEntries.count
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
+        let journalCell = tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath) as! JournalListTableViewCell
+        let journalEntry = sampleJournalEntryData.journalEntries[indexPath.row]
+        journalCell.photoImageView.image = journalEntry.photo
+        journalCell.dateLabel.text = journalEntry.date.formatted(.dateTime.year().month().day())
+        journalCell.titleLabel.text = journalEntry.entryTitle
+        return journalCell
     }
     
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            sampleJournalEntryData.journalEntries.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
+    // MARK: - Methods
     @IBAction func unwindNewEntryCancel(segue: UIStoryboardSegue) {
         
+    }
+    @IBAction func unwindNewEntrySave(segue: UIStoryboardSegue) {
+        if let sourceViewController = segue.source as? AddJournalEntryViewController,
+           let newJournalEntry = sourceViewController.newJournalEntry {
+            sampleJournalEntryData.journalEntries.append(newJournalEntry)
+            tableView.reloadData()
+        } else {
+            print("No Entry or Controller")
+        }
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "entryDetail" else {
+            return
+        }
+        guard let journalEntryDetailViewController = segue.destination as? JournalEntryDetailViewController,
+              let selectedJournalEntryCell = sender as? JournalListTableViewCell,
+              let indexPath = tableView.indexPath(for: selectedJournalEntryCell) else {
+            fatalError("Could not get indexPath")
+        }
+        let selectedJournalEntry = sampleJournalEntryData.journalEntries[indexPath.row]
+        journalEntryDetailViewController.selectedJournalEntry = selectedJournalEntry
     }
 
 }
