42fe7d9fc67bb80647ab11b6044b63977a94e212
DopeKS <kyusang.dev@gmail.com>
2024-05-14 13:31:29 +0900

    위치 정보 업데이트 코드 추가

--- JRNL/JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
+++ JRNL/JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
@@ -43,8 +43,10 @@ class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITe
         let body = bodyTextView.text ?? ""
         let photo = photoImageView.image
         let rating = 3
+        let lat = currentLocation?.coordinate.latitude
+        let long = currentLocation?.coordinate.longitude
         newJournalEntry = JournalEntry(rating: rating, title: title,
-                                       body: body, photo: photo)
+                                       body: body, photo: photo, latitude: lat, longitude: long)
     }
 
     // MARK: - UITextFieldDelegate
@@ -77,10 +79,38 @@ class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITe
         updateSaveButtonState()
     }
     
+    // MARK: - CLLocationManagetDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let myCurrentLocation = locations.first {
+            currentLocation = myCurrentLocation
+            getLocationSwitchLabel.text = "Done"
+            updateSaveButtonState()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+    
     // MARK: - Methods
     private func updateSaveButtonState() {
         let textFieldText = titleTextField.text ?? ""
         let textViewText = bodyTextView.text ?? ""
+        if getLocationSwitch.isOn {
+            saveButton.isEnabled = !textFieldText.isEmpty && !textViewText.isEmpty && currentLocation != nil
+        } else {
+            saveButton.isEnabled = !textFieldText.isEmpty && !textViewText.isEmpty
+        }
+        
         saveButton.isEnabled = !textFieldText.isEmpty && !textViewText.isEmpty
+    }
+    
+    @IBAction func getLacationSwitchValueChanged(_ sender: UISwitch) {
+        if getLocationSwitch.isOn {
+            getLocationSwitchLabel.text = "Getting location..."
+        } else {
+            currentLocation = nil
+            getLocationSwitchLabel.text = "Get Location"
+        }
     }
 }
