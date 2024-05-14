aafcd8cec9ece82ec7709d7965da81a1026c8509
DopeKS <kyusang.dev@gmail.com>
2024-05-14 15:04:31 +0900

    Add Annotations to MapViewController

--- JRNL/JRNL/Map Screen/MapViewController.swift
+++ JRNL/JRNL/Map Screen/MapViewController.swift
@@ -9,10 +9,11 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class MapViewController: UIViewController, CLLocationManagerDelegate {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     @IBOutlet var mapView: MKMapView!
     let locationManager = CLLocationManager()
+    var sampleJournalEntryData = SampleJournalEntryData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,9 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.requestWhenInUseAuthorization()
         self.navigationItem.title = "Loading..."
         locationManager.requestLocation()
+        mapView.delegate = self
+        sampleJournalEntryData.createSampleJournalEntryData()
+        mapView.addAnnotations(sampleJournalEntryData.journalEntries)
     }
     
     // MARK: - CLLocationManagerDelegate
