44c8fe46b930a7ddd31d63f981b8cfa6efe0e99c
DopeKS <kyusang.dev@gmail.com>
2024-05-14 14:38:51 +0900

    MKAnnotation 추가

--- JRNL/JRNL/Journal List Screen/Model/JournalEntry.swift
+++ JRNL/JRNL/Journal List Screen/Model/JournalEntry.swift
@@ -2,12 +2,13 @@
 //  JournalEntry.swift
 //  JRNL
 //
-//  Created by Jungman Bae on 5/10/24.
+//  Created by 황규상 on 5/10/24.
 //
 
 import UIKit
+import MapKit
 
-class JournalEntry {
+class JournalEntry: NSObject, MKAnnotation {
     // MARK: - Properties
     let date: Date
     let rating: Int
@@ -30,6 +31,22 @@ class JournalEntry {
         self.photo = photo
         self.latitude = latitude
         self.longitude = longitude
+    }
+    
+    var coordinate: CLLocationCoordinate2D {
+        guard let lat = latitude,
+              let long = longitude else {
+            return CLLocationCoordinate2D()
+        }
+        return CLLocationCoordinate2D(latitude: lat, longitude: long)
+    }
+    
+    var title: String? {
+        date.formatted(.dateTime.year().month().day())
+    }
+    
+    var subtitle: String? {
+        entryTitle
     }
 }
 
