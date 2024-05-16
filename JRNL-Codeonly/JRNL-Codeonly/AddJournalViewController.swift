cf806c09937b4734db9df190533fd4798aba54eb
DopeKS <kyusang.dev@gmail.com>
2024-05-16 17:29:59 +0900

    뷰 초기화와 설정: 델리게이트 설정, 위치 권한 요청, UI 초기화 등을 수행, setupView() 메서드를 추가하여 UI 초기화를 간소화

--- JRNL-Codeonly/JRNL-Codeonly/AddJournalViewController.swift
+++ JRNL-Codeonly/JRNL-Codeonly/AddJournalViewController.swift
@@ -63,54 +63,23 @@ class AddJournalViewController: UIViewController,UITextFieldDelegate, UITextView
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        titleTextField.delegate = self
+        bodyTextView.delegate = self
+        
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        
         navigationItem.title = "New Entry"
         view.backgroundColor = .white
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save,
-                                                            target: self,
-                                                            action: #selector(save))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(save))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel,
-                                                           target: self,
-                                                           action: #selector(cancel))
-        
-        mainContainer.addArrangedSubview(ratingView)
-        mainContainer.addArrangedSubview(toggleView)
-        mainContainer.addArrangedSubview(titleTextField)
-        mainContainer.addArrangedSubview(bodyTextView)
-        mainContainer.addArrangedSubview(imageView)
-        
-        view.addSubview(self.mainContainer)
-        
-        let safeArea = view.safeAreaLayoutGuide
-        
-        mainContainer.translatesAutoresizingMaskIntoConstraints = false
-        ratingView.translatesAutoresizingMaskIntoConstraints = false
-        toggleView.translatesAutoresizingMaskIntoConstraints = false
-        titleTextField.translatesAutoresizingMaskIntoConstraints = false
-        bodyTextView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            mainContainer.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
-            mainContainer.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20),
-            mainContainer.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20),
-            
-            ratingView.widthAnchor.constraint(equalToConstant: 252),
-            ratingView.heightAnchor.constraint(equalToConstant: 44),
-            
-            titleTextField.leadingAnchor.constraint(equalTo: mainContainer.leadingAnchor, constant: 8),
-            titleTextField.trailingAnchor.constraint(equalTo: mainContainer.trailingAnchor, constant: -8),
-            
-            bodyTextView.leadingAnchor.constraint(equalTo: mainContainer.leadingAnchor, constant: 8),
-            bodyTextView.trailingAnchor.constraint(equalTo: mainContainer.trailingAnchor, constant: -8),
-            bodyTextView.heightAnchor.constraint(equalToConstant: 128),
-            
-            imageView.widthAnchor.constraint(equalToConstant: 200),
-            imageView.heightAnchor.constraint(equalToConstant: 200)
-            
-        ])
-        
+        setupView()
+    }
+    
+    private func setupView() {
+        photoImageView.image = UIImage(systemName: "face.smiling")
     }
     
     @objc func save() {
@@ -126,14 +95,5 @@ class AddJournalViewController: UIViewController,UITextFieldDelegate, UITextView
     @objc func cancel() {
         dismiss(animated: true)
     }
-
-    /*
-    // MARK: - Navigation
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
