a45813697382e032f7976009fb09d97126eb4e3e
DopeKS <kyusang.dev@gmail.com>
2024-05-16 09:28:02 +0900

    Add SharedData(싱글톤)

---
+++ JRNL/JRNL/Model/SharedData.swift
@@ -0,0 +1,38 @@
+//
+//  SharedData.swift
+//  JRNL
+//
+//  Created by 황규상 on 5/16/24.
+//
+
+import UIKit
+
+class SharedData {
+    static let shared = SharedData()
+    private var journalEntries: [JournalEntry]
+    
+    private init() {
+        journalEntries = []
+    }
+    
+    func numberOfJournalEntries() -> Int {
+        journalEntries.count
+    }
+    
+    func getJournalEntry(index: Int) -> JournalEntry {
+        journalEntries[index]
+    }
+    
+    func getAllJournalEntries() -> [JournalEntry] {
+        let readOnlyJournalEntries = journalEntries
+        return readOnlyJournalEntries
+    }
+    
+    func addJournalEntry(newJournalEntry: JournalEntry) {
+        journalEntries.append(newJournalEntry)
+    }
+    
+    func removeJournalEntry(index: Int) {
+        journalEntries.remove(at: index)
+    }
+}
