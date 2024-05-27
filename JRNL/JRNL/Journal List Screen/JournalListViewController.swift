31d0135d371bff00dba94520ef4d511360a8d497
DopeKS <kyusang.dev@gmail.com>
2024-05-27 16:44:34 +0900

    journalCell connections 설정

--- JRNL/JRNL/Journal List Screen/JournalListViewController.swift
+++ JRNL/JRNL/Journal List Screen/JournalListViewController.swift
@@ -34,7 +34,7 @@ class JournalListViewController: UIViewController, UITableViewDataSource, UITabl
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let journalCell = tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath) as! JournalListTableViewCell
+        let journalCell = tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath) as! JournalListCollectionViewCell
         let journalEntry: JournalEntry
         if self.search.isActive {
             journalEntry = filteredTableData[indexPath.row]
@@ -59,7 +59,7 @@ class JournalListViewController: UIViewController, UITableViewDataSource, UITabl
                 SharedData.shared.removeSelectedJournalEntry(selectedJournalEntry)
             } else {
                 SharedData.shared.removeJournalEntry(index: indexPath.row)
-            }            
+            }
             SharedData.shared.saveJournalEntriesData()
             tableView.reloadData()
         }
@@ -109,7 +109,7 @@ class JournalListViewController: UIViewController, UITableViewDataSource, UITabl
             return
         }
         guard let journalEntryDetailViewController = segue.destination as? JournalEntryDetailViewController,
-              let selectedJournalEntryCell = sender as? JournalListTableViewCell,
+              let selectedJournalEntryCell = sender as? JournalListCollectionViewCell,
               let indexPath = tableView.indexPath(for: selectedJournalEntryCell) else {
             fatalError("Could not get indexPath")
         }
