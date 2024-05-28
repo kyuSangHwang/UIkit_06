e2b913cb7224fab019a9b3574b2039d7bf2c4945
DopeKS <kyusang.dev@gmail.com>
2024-05-28 13:10:35 +0900

    NSObject 삭제: SwiftData 빌드 오류 해결

--- JRNL/JRNL/Model/JournalEntry.swift
+++ JRNL/JRNL/Model/JournalEntry.swift
@@ -10,13 +10,13 @@ import MapKit
 import SwiftData
 
 @Model
-class JournalEntry: NSObject, MKAnnotation {
+class JournalEntry {
     // MARK: - Properties
     let dateString: String
     let rating: Int
     let entryTitle: String
     let entryBody: String
-    let photoData: Data?
+    @Attribute(.externalStorage) let photoData: Data?
     let latitude: Double?
     let longitude: Double?
     
@@ -37,46 +37,19 @@ class JournalEntry: NSObject, MKAnnotation {
         self.longitude = longitude
     }
     
-    var coordinate: CLLocationCoordinate2D {
-        guard let lat = latitude,
-              let long = longitude else {
-            return CLLocationCoordinate2D()
-        }
-        return CLLocationCoordinate2D(latitude: lat, longitude: long)
-    }
-    
-    var title: String? {
-        dateString
-    }
-    
-    var subtitle: String? {
-        entryTitle
-    }
-}
-
-// MARK: - Sample data
-struct SampleJournalEntryData {
-    var journalEntries: [JournalEntry] = []
-    
-    mutating func createSampleJournalEntryData() {
-        let photo1 = UIImage(systemName: "sun.max")
-        let photo2 = UIImage(systemName: "cloud")
-        let photo3 = UIImage(systemName: "cloud.sun")
-        guard let journalEntry1 = JournalEntry(rating: 5, title: "Good",
-                                               body: "Today is good day", photo: photo1) else {
-            fatalError("Unable to instantiate journalEntry1")
-        }
-        guard let journalEntry2 = JournalEntry(rating: 0, title: "Bad",
-                                               body: "Today is bad day", photo: photo2,
-                                               latitude: 37.56661, longitude: 126.978388) else {
-            fatalError("Unable to instantiate journalEntry2")
-        }
-        guard let journalEntry3 = JournalEntry(rating: 3, title: "Ok",
-                                               body: "Today is Ok day", photo: photo3) else {
-            fatalError("Unable to instantiate journalEntry3")
-        }
-        
-        journalEntries += [journalEntry1, journalEntry2, journalEntry3]
-        
-    }
+//    var coordinate: CLLocationCoordinate2D {
+//        guard let lat = latitude,
+//              let long = longitude else {
+//            return CLLocationCoordinate2D()
+//        }
+//        return CLLocationCoordinate2D(latitude: lat, longitude: long)
+//    }
+//    
+//    var title: String? {
+//        dateString
+//    }
+//    
+//    var subtitle: String? {
+//        entryTitle
+//    }
 }
