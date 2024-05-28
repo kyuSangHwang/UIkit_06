b860b615739f77593acc519231eb80b4e1342716
DopeKS <kyusang.dev@gmail.com>
2024-05-28 17:09:02 +0900

    Tab 뷰 추가

---
+++ JRNL-SwiftUI/JRNL-SwiftUI/JournalListView.swift
@@ -0,0 +1,18 @@
+//
+//  JournalListView.swift
+//  JRNL-SwiftUI
+//
+//  Created by 황규상 on 5/28/24.
+//
+
+import SwiftUI
+
+struct JournalListView: View {
+    var body: some View {
+        Text("Journal List")
+    }
+}
+
+#Preview {
+    JournalListView()
+}
---
+++ JRNL-SwiftUI/JRNL-SwiftUI/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  JRNL-SwiftUI
+//
+//  Created by 황규상 on 5/28/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+#Preview {
+    MapView()
+}
