e46f2a5fe8ac21c2a7761fe2b3fdeae1b2214ef1
DopeKS <kyusang.dev@gmail.com>
2024-05-29 09:30:37 +0900

    RatingView 추가

--- JRNL-SwiftUI/JRNL-SwiftUI/AddJournalEntryView.swift
+++ JRNL-SwiftUI/JRNL-SwiftUI/AddJournalEntryView.swift
@@ -7,10 +7,28 @@
 
 import SwiftUI
 
+struct RatingView: View {
+    @Binding var rating: Int
+    
+    var body: some View {
+        HStack {
+            ForEach (0..<5) { index in
+                Image(systemName: index < rating ? "star.fill" : "star")
+                    .foregroundColor(.yellow)
+                    .onTapGesture {
+                        rating = index + 1
+                    }
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .center)
+    }
+}
+
 struct AddJournalEntryView: View {
     @Environment(\.modelContext) var modelContext
     @Environment(\.dismiss) var dismiss
     
+    @State private var rating:Int = 0
     @State private var isGetLocationOn:Bool = false
     @State private var entryTitle:String = ""
     @State private var entryBody:String = ""
@@ -19,8 +37,7 @@ struct AddJournalEntryView: View {
         NavigationStack {
             Form {
                 Section(header: Text("Rating")) {
-                    Rectangle()
-                        .foregroundStyle(.cyan)
+                    RatingView(rating: $rating)
                 }
                 Section(header: Text("Location")) {
                     Toggle("Get Location", isOn: $isGetLocationOn)
