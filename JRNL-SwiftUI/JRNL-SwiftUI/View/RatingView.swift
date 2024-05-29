48c6ac438c0a7d2299900f80628290a8ff577b70
DopeKS <kyusang.dev@gmail.com>
2024-05-29 10:26:34 +0900

    포토 라이브러리 추가

--- JRNL-SwiftUI/JRNL-SwiftUI/View/RatingView.swift
+++ JRNL-SwiftUI/JRNL-SwiftUI/View/RatingView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct RatingView: View {
     @Binding var rating: Int
-
+    
     var body: some View {
         HStack {
             ForEach(0..<5) { index in
                 Image(systemName: index < rating ? "star.fill" : "star")
-                    .foregroundColor(.yellow)
+                    .foregroundColor(.blue)
                     .onTapGesture {
                         rating = index + 1
                     }
