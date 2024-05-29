f976a732508b17937dc303468f44054628ed00eb
DopeKS <kyusang.dev@gmail.com>
2024-05-29 10:46:08 +0900

    디테일 페이지 추가

---
+++ JRNL-SwiftUI/JRNL-SwiftUI/JouranlEntryDetailView.swift
@@ -0,0 +1,47 @@
+//
+//  JouranlEntryDetailView.swift
+//  JRNL-SwiftUI
+//
+//  Created by 황규상 on 5/29/24.
+//
+
+import SwiftUI
+
+struct JournalEntryDetailView: View {
+    var journalEntry: JournalEntry
+    @State private var mapImage: UIImage?
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Spacer().frame(height: 30)
+                Text(journalEntry.date.formatted(.dateTime.year().month().day()))
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                Spacer().frame(height: 30)
+                Text(journalEntry.entryTitle)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer().frame(height: 30)
+                Text(journalEntry.entryBody)
+                    .font(.title2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer().frame(height: 30)
+                Image(uiImage: journalEntry.photo ?? UIImage(systemName: "face.smiling")!)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 300, height: 300)
+                    .clipped()
+                Spacer().frame(height: 30)
+                Image(uiImage: mapImage ?? UIImage(systemName: "map")!)
+                    .resizable()
+                    .frame(width: 300, height: 300)
+            }
+            .padding()
+        }
+        .navigationTitle("Entry Detail")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
