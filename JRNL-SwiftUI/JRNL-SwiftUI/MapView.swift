cb470c0a9be27bcec7998e625e2bc65f9f56a5a8
DopeKS <kyusang.dev@gmail.com>
2024-05-29 11:25:47 +0900

    MapView 추가 (UIViewRepresentable)

--- JRNL-SwiftUI/JRNL-SwiftUI/MapView.swift
+++ JRNL-SwiftUI/JRNL-SwiftUI/MapView.swift
@@ -6,10 +6,55 @@
 //
 
 import SwiftUI
+import MapKit
+import CoreLocation
+
+struct MapUIView: UIViewRepresentable {
+    @Binding var region: MKCoordinateRegion
+    @Binding var annotations: [MKAnnotation]
+    @Binding var journalEntry: JournalEntry?
+    @Binding var isDetailViewActive: Bool
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        uiView.setRegion(region, animated: true)
+        uiView.addAnnotations(annotations)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapUIView
+        init(_ parent: MapUIView) {
+            self.parent = parent
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: any MKAnnotation) -> MKAnnotationView? {
+            return nil
+        }
+    }
+    
+}
 
 struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
+        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    @State private var annotations: [MKAnnotation] = []
+    @State private var journalEntry: JournalEntry?
+    @State private var isDetailViewActive = false
+
     var body: some View {
-        Text("Map")
+        NavigationStack {
+            MapUIView(region: $region, annotations: $annotations, journalEntry: $journalEntry, isDetailViewActive: $isDetailViewActive)
+        }
     }
 }
 
